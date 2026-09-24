451f8cccca14eaab375f9e5b106190b1549c4869
aravindmohan-mcb <aravindmohan@am.amrita.edu>
2026-09-24 13:06:11 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Site settings. Fill these in after following SETUP.md.
+window.CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbz3z8xlXwzVV3nd4pwzZ9q0ggm7HUrvTNXC2xmejZVaY5gPj1dSxPE3t3CfZ8544DPB/exec',       // ends with /exec
+  GOOGLE_CLIENT_ID: '862708751615-elt639734po2gsgue2cae1jsggheo1e2.apps.googleusercontent.com',
+  SITE_NAME: 'Amrita MCB Events',
+  DEV_MODE: false // leave false. Only used for local testing.
+};
